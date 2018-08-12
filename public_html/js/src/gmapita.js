c34fa693a5b9085922cf3ee22ec79b224bdf4d24
dmase44 <dmase@iteka.com>
2018-08-12 11:04:31 -0500

    Initial commit to Iteka repo

---
+++ public_html/js/src/gmapita.js
@@ -0,0 +1,4 @@
+<script src="http://maps.google.com/maps/api/js?key=AIzaSyBP9iyGYl9okc4X9o9wQclSL0f1NKXdR_4&callback=initMap"
+async defer></script>
+<script src="js/src/jquery.gmap3.min.js"></script>
+<script>function itaMap(){$('.itaGmap').gmap3({marker:{address:'312 VJ Cryer Road. Huntington Texas 75949'},map:{options:{zoom:9,scrollwheel:false,streetViewControl:true}}});}</script>
